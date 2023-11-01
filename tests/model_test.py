3382c59d23a5f6f7da78a2ee5b8190ad8b8146e5
Kat Sukkau <Katsukkau@gmail.com>
2023-11-01 09:41:55 -0700

    added test files for all backend named functions in model test

---
+++ tests/model_test.py
@@ -0,0 +1,16 @@
+import pytest
+from ..backend.model import BaseStats, NameTranslations, PokemonModel
+
+class ModelTest:
+
+    @staticmethod
+    def test_pokemon_model_validation():
+        pass
+
+    @staticmethod
+    def test_pokemon_model_serialization():
+        pass
+
+    @staticmethod
+    def test_name_translations():
+        pass
