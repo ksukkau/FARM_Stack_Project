6772eda85bdf5c55b97093ca36bbc80b44b3777a
Kat Sukkau <Katsukkau@gmail.com>
2023-11-08 09:00:38 -0800

    updated imports on test_database

--- tests/database_test.py
+++ tests/database_test.py
@@ -1,2 +1,10 @@
+import os
+import sys
+import pytest_asyncio
+import asyncio
+
 import pytest
-from ..backend.database import fetch_one_pokemon, fetch_all_pokemon, create_pokemon, update_pokemon, delete_pokemon
+current_dir = os.path.dirname(os.path.realpath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
+sys.path.append(project_root)
+from backend.database import fetch_one_pokemon, fetch_all_pokemon, create_pokemon, update_pokemon, upsert_pokemon_db, delete_pokemon_from_db
