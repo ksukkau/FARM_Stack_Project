f721a49886a93fe7e8a3a91df57f5702dea9313b
Kat Sukkau <Katsukkau@gmail.com>
2023-11-18 10:36:52 -0800

    added endpoint to reload database with original data

--- backend/database.py
+++ backend/database.py
@@ -9,10 +9,11 @@ Author: Kat Sukkau
 Date: Oct 2023
 """
 from decouple import config
-from backend.model import PokemonModel
+from model import PokemonModel
+import httpx
 #mongoDB driver
 import motor.motor_asyncio
-
+POKE_URL = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
 
 #testing database
 client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
@@ -77,3 +78,43 @@ async def delete_pokemon_from_db(p_id: int):
         return {"message": f"Deletion of Pokemon {p_id} successful"}
     else:
         return {"message": "No document found for deletion"}
+
+async def reload_pokemon_db():
+    """Delete all pokemon reload fresh data from source"""
+    deleted = await collection.delete_many({})
+    if deleted:
+        data = await fetch_data_from_url(POKE_URL)
+        if data:
+            validated_data = validate_data(data)
+            try:
+                await collection.insert_many(validated_data)
+                return "Insert operation successful"
+            except Exception as e:
+                return f"Insert operation failed with error: {str(e)}"
+    return "No data to insert"
+
+async def fetch_data_from_url(url):
+    """Fetch Pokemon List from Source"""
+    async with httpx.AsyncClient() as client2:
+        response = await client2.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        response.raise_for_status()
+
+def rename_keys(document):
+    """Rename Keys to match model"""
+    if "Sp. Attack" in document["base"]:
+        document["base"]["Special Attack"] = document["base"].pop("Sp. Attack")
+    if "Sp. Defense" in document["base"]:
+        document["base"]["Special Defense"] = document["base"].pop("Sp. Defense")
+
+def validate_data(data):
+    """Validate data against model"""
+    for doc in data:
+        rename_keys(doc)
+    validated_data = [PokemonModel(**item) for item in data]
+    if validated_data:
+        # convert back to dict to insert into database
+        validated_data = [item.dump() for item in validated_data]
+    return validated_data
--- backend/main.py
+++ backend/main.py
@@ -21,8 +21,8 @@ Date: Oct 2023
 """
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from backend.database import fetch_one_pokemon, fetch_all_pokemon, create_pokemon, update_pokemon, delete_pokemon_from_db, upsert_pokemon_db
-from backend.model import PokemonModel
+from database import fetch_one_pokemon, fetch_all_pokemon, create_pokemon, update_pokemon, delete_pokemon_from_db, upsert_pokemon_db, delete_pokemon_from_db, reload_pokemon_db
+from model import PokemonModel
 
 
 app = FastAPI()
@@ -43,78 +43,88 @@ app.add_middleware(
 def read_root():
   return {"Hello": "World"}
 
+@app.post("/api/v1/pokemon/reload")
+async def reload_pokemon():
+    try:
+        result = await reload_pokemon_db()
+        if result:
+            return {'message': f'{result}'}
+        raise HTTPException(status_code=500, detail="Failed to reload the Pokémon")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
 @app.get("/api/v1/pokemon/{id}")
 async def get_pokemon_by_id(p_id:int):
-  if not p_id:
-    raise HTTPException(status_code=400, detail="Please enter an integer")
-  response = await fetch_one_pokemon(p_id)
-  if response:
-    return response
-  raise HTTPException(status_code=404, detail=f"Pokémon with ID {p_id} not found")
+    if not p_id:
+        raise HTTPException(status_code=400, detail="Please enter an integer")
+    response = await fetch_one_pokemon(p_id)
+    if response:
+        return response
+    raise HTTPException(status_code=404, detail=f"Pokémon with ID {p_id} not found")
 
 @app.get("/api/v1/allpokemon")
 async def get_all_pokemon():
-  response = await fetch_all_pokemon()
-  if response:
-    return response
-  raise HTTPException(status_code=400, detail="Oops something went wrong / Bad Request")
+    response = await fetch_all_pokemon()
+    if response:
+        return response
+    raise HTTPException(status_code=400, detail="Oops something went wrong / Bad Request")
 
 @app.post("/api/v1/pokemon/", response_model=PokemonModel)
 async def post_pokemon(pokemon: PokemonModel):
-  try:
-    result = await create_pokemon(pokemon)
-    print(type(result))
-    if result:
-      return result
-    raise HTTPException(status_code=500, detail="Failed to create the Pokémon")
-  except Exception as e:
-    raise HTTPException(status_code=400, detail=str(e))
+    try:
+        result = await create_pokemon(pokemon)
+        print(type(result))
+        if result:
+            return result
+        raise HTTPException(status_code=500, detail="Failed to create the Pokémon")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 @app.put("/api/v1/pokemon/{id}", response_model=PokemonModel)
 async def put_pokemon(p_id: int, pokemon: PokemonModel):
-  if not p_id:
-    raise HTTPException(status_code=400, detail="Please enter an integer ID")
+    if not p_id:
+        raise HTTPException(status_code=400, detail="Please enter an integer ID")
 
-  try:
-    # Attempt to update the Pokemon in the database
-    result = await update_pokemon(p_id, pokemon)
+    try:
+        # Attempt to update the Pokemon in the database
+        result = await update_pokemon(p_id, pokemon)
 
-    # Check if the update was acknowledged and return the updated Pokemon
-    if result.acknowledged:
-      updated_pokemon = await fetch_one_pokemon(p_id)
-      if updated_pokemon:
-        return updated_pokemon
-    raise HTTPException(status_code=404, detail=f"Pokemon with ID {p_id} not found")
+        # Check if the update was acknowledged and return the updated Pokemon
+        if result.acknowledged:
+            updated_pokemon = await fetch_one_pokemon(p_id)
+        if updated_pokemon:
+            return updated_pokemon
+        raise HTTPException(status_code=404, detail=f"Pokemon with ID {p_id} not found")
 
-  except Exception as e:
-    raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 @app.post("/api/v1/pokemon/{id}", response_model=PokemonModel)
 async def upsert_pokemon(p_id: int, pokemon: PokemonModel):
-  if not p_id:
-    raise HTTPException(status_code=400, detail="Please enter an integer ID")
-  try:
-    result = await upsert_pokemon_db(p_id, pokemon)
-    if result.acknowledged:
-      updated_pokemon = await fetch_one_pokemon(p_id)
-      if updated_pokemon:
-        return updated_pokemon
-    raise HTTPException(status_code=404, detail=f"Pokemon with ID {p_id} not found")
+    if not p_id:
+        raise HTTPException(status_code=400, detail="Please enter an integer ID")
+    try:
+        result = await upsert_pokemon_db(p_id, pokemon)
+        if result.acknowledged:
+            updated_pokemon = await fetch_one_pokemon(p_id)
+        if updated_pokemon:
+            return updated_pokemon
+        raise HTTPException(status_code=404, detail=f"Pokemon with ID {p_id} not found")
 
-  except Exception as e:
-    raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 
 @app.delete("/api/v1/pokemon/{id}")
 async def delete_pokemon(p_id: int):
-  if not p_id:
-    raise HTTPException(status_code=400, detail="Please enter an integer ID")
+    if not p_id:
+        raise HTTPException(status_code=400, detail="Please enter an integer ID")
 
-  try:
-    response = await delete_pokemon_from_db(p_id)
-    if response:
-      return response
-    raise HTTPException(status_code=404, detail=f"Pokemon with ID {p_id} not found")
-  except Exception as e:
-    raise HTTPException(status_code=400, detail=str(e))
+    try:
+        response = await delete_pokemon_from_db(p_id)
+        if response:
+            return {'message': 'Database update successful'}, 200
+        raise HTTPException(status_code=404, detail=f"Pokemon with ID {p_id} not found")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
